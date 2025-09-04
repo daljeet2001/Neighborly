9e7ca458766f51c8b3724825e8c923dd28107ada
DALJEET SINGH <daljeetsingh@DALJEETs-MacBook-Air.local>
2025-09-04 13:59:00 +0530

    responsive cards

--- neighborly/app/page.tsx
+++ neighborly/app/page.tsx
@@ -25,32 +25,26 @@ export default function LandingPage() {
     <div className="w-full max-w-7xl">
     
       <AuthCard />
-      <div className='flex gap-4 w-full'>
-        <FeatureCard
+    <div className="flex flex-col gap-4 w-full md:flex-row md:flex-wrap">
+  <FeatureCard
+    bgImage="https://d19rpgkrjeba2z.cloudfront.net/static/gen/9177d51f4d2aeb7aa922.jpg"
+    mainImage="https://images.unsplash.com/photo-1698768194564-bd8ac2f4ea94?w=900&auto=format&fit=crop&q=60"
+    description="Find and connect with nearby services."
+  />
 
-   
-  bgImage="https://d19rpgkrjeba2z.cloudfront.net/static/gen/9177d51f4d2aeb7aa922.jpg"
-  mainImage="https://images.unsplash.com/photo-1698768194564-bd8ac2f4ea94?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzl8fGluZGlhbiUyMG1lY2hhbmljc3xlbnwwfHwwfHx8MA%3D%3D"
+  <FeatureCard
+    bgImage="https://d19rpgkrjeba2z.cloudfront.net/static/gen/4ed7dbd4f62b537ce06f.jpg"
+    mainImage="https://images.unsplash.com/photo-1588925511355-114811363a2c?w=900&auto=format&fit=crop&q=60"
+    description="Stay informed with alerts and local news."
+  />
 
-  description="Find and connect with nearby services."
-/>
+  <FeatureCard
+    bgImage="https://d19rpgkrjeba2z.cloudfront.net/static/gen/521fca1698691c4dcdf9.jpg"
+    mainImage="https://images.unsplash.com/photo-1621334721541-370a13974de8?q=80&w=987&auto=format&fit=crop"
+    description="Discover local favorites by neighbors."
+  />
+</div>
 
-        <FeatureCard
-  bgImage="https://d19rpgkrjeba2z.cloudfront.net/static/gen/4ed7dbd4f62b537ce06f.jpg"
-  mainImage="https://images.unsplash.com/photo-1588925511355-114811363a2c?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjR8fHRyYWZmaWMlMjBjb25lJTIwYmFycmllcnxlbnwwfHwwfHx8MA%3D%3D"
-
-  description="Stay informed with alerts and local news."
-/>
-        <FeatureCard
-  bgImage="https://d19rpgkrjeba2z.cloudfront.net/static/gen/521fca1698691c4dcdf9.jpg"
-  mainImage="https://images.unsplash.com/photo-1621334721541-370a13974de8?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-
-  description="Discover local favorites by neighbors."
-/>
-     
-
-
-      </div>
       <CTASection />
     </div>
   );
